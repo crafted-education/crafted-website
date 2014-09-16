d98e66d0bb85eb718defa1e4c9a49e12d0198b8d
Matt Self <self.matt@gmail.com>
2014-09-15 18:50:48 -0600

    port adjustment, for docker

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("web/static"))))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
