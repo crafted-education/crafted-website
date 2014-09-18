05777bc30699cd94d5f473f5ba79d75a163818a4
Matt Self <self.matt@gmail.com>
2014-09-17 21:55:44 -0600

    added about us page

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("web/static"))))
+
+	//	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("web/static/test"))))
+
 	if err := http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
