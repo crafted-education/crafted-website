9a1511ffa0b42d6c8e7895f865f9fe72dbc21821
Matt Self <self.matt@gmail.com>
2014-10-07 21:23:28 -0600

    fixes based on comments

--- web/static/2/assets/js/custom.js
+++ web/static/2/assets/js/custom.js
@@ -45,4 +45,13 @@ jQuery(function($) {
     WRITE  YOUR  SCRIPTS BELOW
     =====================================================*/
 
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
 });
